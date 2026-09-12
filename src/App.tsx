8c6318bf407a463298bba78e86062bb0e03f3536
FaWayo <wayofatima@gmail.com>
2026-09-12 15:20:55 +0000

    Revert "feat: add Terms of Service page"
    This reverts commit f6b57f2ece28565b7692432fde998cbf221b7ca2.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,6 @@ import {
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
-import TermsOfService from "./pages/TermsOfService";
 import SetupPassword from "./pages/SetupPassword";
 import ChangePassword from "./pages/ChangePassword";
 import { AppShell } from "./components/layout/AppShell";
@@ -161,7 +160,6 @@ export default function App() {
                   <Route path="/forgot-password" element={<ForgotPassword />} />
                   <Route path="/change-password" element={<ChangePassword />} />
                   <Route path="/privacy" element={<PrivacyPolicy />} />
-                  <Route path="/terms" element={<TermsOfService />} />
                   <Route path="/docs" element={gatedDocs} />
                   <Route path="*" element={<Navigate to="/docs" replace />} />
                 </SentryRoutes>
@@ -174,7 +172,6 @@ export default function App() {
                   <Route path="/activate" element={<SetupPassword />} />
                   <Route path="/reset" element={<SetupPassword />} />
                   <Route path="/privacy" element={<PrivacyPolicy />} />
-                  <Route path="/terms" element={<TermsOfService />} />
                   <Route path="/change-password" element={<ChangePassword />} />
 
                   {/* Protected app */}
